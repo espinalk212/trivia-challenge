ada0e06d8f0fc0c3de30e96ad7b0951f0ff9481a
espinalk212 <espinalk212@gmail.com>
2020-11-01 15:55:31 -0500

    refactor footer

--- src/Footer.js
+++ src/Footer.js
@@ -4,7 +4,8 @@ import './Footer.css';
 function Footer() {
   return (
     <div className="footer__container">
-      <a href="https://www.freevector.com/pastel-background-30546">FreeVector.com</a>
+      <p>Made with ❤️ by Kevin Espinal. Copyright 2020</p>
+      <p>Wallpaper:<a href="https://www.freevector.com/pastel-background-30546">FreeVector.com</a></p>
     </div>
   )
 }
