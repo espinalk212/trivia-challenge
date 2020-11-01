01a7b3ec145d3b6c13fa6ed17d843bc4c0eb61cd
espinalk212 <espinalk212@gmail.com>
2020-11-01 16:26:33 -0500

    refactor Footer.js

--- src/Footer.js
+++ src/Footer.js
@@ -1,13 +1,29 @@
-import React from 'react';
-import './Footer.css';
+import React from "react";
+import "./Footer.css";
 
 function Footer() {
   return (
     <div className="footer__container">
       <p>Made with ❤️ by Kevin Espinal. Copyright 2020</p>
-      <p>Wallpaper:<a href="https://www.freevector.com/pastel-background-30546">FreeVector.com</a></p>
+      <p>
+        <a
+          href="https://github.com/espinalk212/trivia-challenge"
+          target="_blank"
+          rel="noreferrer"
+        >Source Code
+        </a>
+      </p>
+      <p>
+        
+        <a
+          href="https://www.freevector.com/pastel-background-30546"
+          target="_blank"
+          rel="noreferrer"
+        >Wallpaper
+        </a>
+      </p>
     </div>
-  )
+  );
 }
 
-export default Footer
+export default Footer;
